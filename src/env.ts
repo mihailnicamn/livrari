2f32f9fbfae719a3d71d7adaa730cc1c860f175c
mihail-nica <146497292+mihail-nica@users.noreply.github.com>
2024-03-25 04:28:38 +0200

    modified:   src/env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,2 @@
-// export const API = "https://livrari-back.onrender.com";
-export const API = "http://192.168.1.131:3000";
+export const API = "https://livrari-back-livrari.koyeb.app";
+// export const API = "http://192.168.1.131:3000";
